ddf27d91f80b3a7a9b23b8c86cee8bbfc44f56c4
Avraj Singh <ASingh@spartaglobal.com>
2020-02-07 14:52:31 +0000

    monster and student class

---
+++ Monster_class.py
@@ -0,0 +1,19 @@
+
+class Monster():
+
+    def __init__(self, name, strength_level, ):
+        self.name = name
+        self.strength_level = strength_level
+        self.scary_skill = self.scary_skill
+
+    def sleep(self):
+        return 'zzzzzzzz'
+    def eat(self):  # To make args optional (arg = '')
+        return 'nom nom nom nom'
+    def pay_taxes(self):
+        return 'Where my money Brian'
+    def shout_strength(self):
+        return 'RRRRRRRRRRRRRRRAAAAAAAAAAAAAAAAAA!!!, mmmeeuuuu'
+
+
+
---
+++ Student_Monster_class.py
@@ -0,0 +1,9 @@
+from Monster_class import Monster
+class Student_Monster(Monster):
+
+    def uni_id(self):
+        return 'u1612865'
+    def scary_subject(self):
+        return 'Jump scare'
+    def scare_strat(self):
+        return ''
